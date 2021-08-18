05b3e78ccd02130fe2aaf05d489d0ec752d7ec36
Lint Action <lint-action@samuelmeuli.com>
2021-08-18 21:54:23 +0000

    Fix code style issues with Black

--- test/test_pt_client.py
+++ test/test_pt_client.py
@@ -1,8 +1,8 @@
 import unittest
 from pt_client import NamedEntityClient
 
-class TestPtClient(unittest.TestCase):
 
+class TestPtClient(unittest.TestCase):
     def test_get_ents_returns_dictionary_given_empty_string(self):
         ner = NamedEntityClient()
         ents = ner.get_ents("")
