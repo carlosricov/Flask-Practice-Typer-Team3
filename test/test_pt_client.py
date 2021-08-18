5ae62080e241271a496bcea5c4d1ab76a433c02a
CarlosHurand <carlos_hurand@hotmail.com>
2021-08-18 16:53:56 -0500

    Pre testing

---
+++ test/test_pt_client.py
@@ -0,0 +1,14 @@
+import unittest
+from pt_client import NamedEntityClient
+
+class TestPtClient(unittest.TestCase):
+
+    def test_get_ents_returns_dictionary_given_empty_string(self):
+        ner = NamedEntityClient()
+        ents = ner.get_ents("")
+        self.assertIsInstance(ents, dict)
+
+    def test_get_ents_returns_dictionary_given_empty_string(self):
+        ner = NamedEntityClient()
+        ents = ner.get_ents("Madison is a city in Wisconsin")
+        self.assertIsInstance(ents, dict)
