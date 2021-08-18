282194057b8be6c59c34123acf44eb630c9d1ecb
Lint Action <lint-action@samuelmeuli.com>
2021-08-18 23:54:36 +0000

    Fix code style issues with Black

--- test/test_api.py
+++ test/test_api.py
@@ -4,29 +4,29 @@ from werkzeug.wrappers import response
 
 from app import app
 
+
 class TestApi(unittest.TestCase):
-    
     def test_pt_endpoint_health_returns_200(self):
         with app.test_client() as client:
-            response = client.get('/health')
+            response = client.get("/health")
             assert response._status_code == 200
-    
+
     def test_pt_endpoint_login_get_returns_200(self):
         with app.test_client() as client:
-            response = client.get('/login')
+            response = client.get("/login")
             assert response._status_code == 200
 
     def test_pt_endpoint_login_post_returns_418(self):
         with app.test_client() as client:
-            response = client.post('/login')
+            response = client.post("/login")
             assert response._status_code == 418
 
     def test_pt_endpoint_register_get_returns_200(self):
         with app.test_client() as client:
-            response = client.get('/register')
+            response = client.get("/register")
             assert response._status_code == 200
 
     def test_pt_endpoint_register_post_returns_418(self):
         with app.test_client() as client:
-            response = client.post('/register')
+            response = client.post("/register")
             assert response._status_code == 418
