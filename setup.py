d26695c792bac1aa0507929effb1aeac4c0f8936
CarlosHurand <carlos_hurand@hotmail.com>
2021-08-19 22:38:28 -0500

    Updated version on setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages
 
 setup(
     name="PracticeTyper",
-    version="0.9.0",
+    version="1.0.0",
     description="A Flask application dedicated to improving your programming typing speed.",
 )
